9a17f6ae3ec37b2c7490116c3c9244170038b203
gabriel200217 <gabrielchacon200269@gmail.com>
2023-05-01 19:09:58 -0400

    1-5-2023 commit

--- js/videos.js
+++ js/videos.js
@@ -1,5 +1,3 @@
-function agregarVideo() {
-    var videoUrl = "https://www.youtube.com/embed/Z5NoQg8LdDk";
-    var embedElement = document.getElementById("youtube-embed");
-    embedElement.innerHTML = '<iframe width="560" height="315" src="' + videoUrl + '" frameborder="0" allowfullscreen></iframe>';
-  }
+$(document).ready(function() {
+  $('#example').DataTable();
+} );
